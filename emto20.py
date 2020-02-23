88af2babd3d3c4024141fc8bc9e886ddee37ce4e
1563937467 <1563937467@didichuxing.com>
2020-02-23 21:03:20 +0800

    1、字符串替换（非原字符串做修改）

--- emto20.py
+++ emto20.py
@@ -3,7 +3,7 @@
 #请实现一个函数，将一个字符串中的每个空格替换成“%20”。例如，当字符串为We Are Happy.则经过替换之后的字符串为We%20Are%20Happy。
 class Solution:
     # s 源字符串
-    def replaceSpace(self, s):
+    def replaceSpace(self, s):#开辟新空间，非原字符串上做修改
         #    write code here
         tg1 = "%20"
         tg2 = " "
